59fef73c5cfd096c07926e0a4271135bd22943b1
StanLong <youlinbhz@sina.cn>
2019-08-12 23:55:11 +0800

    mybatis一级缓存和二级缓存

---
+++ mybatis04/src/main/java/com/stanlong/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,31 @@
+package com.stanlong.mybatis.dao;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
+
+import com.stanlong.mybatis.pojo.Employee;
+
+public interface EmployeeMapper {
+	
+	public Employee getEmpById(Integer id);
+
+	public Employee getEmpMap(Map<String, Object> map);
+	
+	public Employee getEmpByIdAndLastName(@Param("id") Integer id, @Param("lastName") String lastName);
+
+	public void addEmp(Employee employee);
+
+	public void updateEmp(Employee employee);
+
+	public void deleteById(Integer id);
+	
+	//返回一条记录的map， key就是列名，值就是对应 的值
+	public Map<String, Object> getEmpByIdReturnMap(Integer id);
+	
+	//返回多条记录的map， key是对应的主键，值就是封装后的java对象
+	//@MapKey 告诉mybatis封装map的时候使用哪个属性作为主键
+	@MapKey("id")
+	public Map<String, Object> getEmpByLastNameLikeReturnMap(String lastName);
+}
