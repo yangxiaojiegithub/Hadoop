e33f7c27ebbe19fc6aac779fc181867fbdbeb6d8
StanLong <youlinbhz@sina.cn>
2019-08-17 00:36:54 +0800

    springmvc没有拦截成功

--- mybatis05_ssm/WebContent/index.jsp
+++ mybatis05_ssm/WebContent/index.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>查询所有员工</title>
+<title>Insert title here</title>
 </head>
 <body>
-	<a href="emps">查询所有员工</a>
+	<a href="getEmps">查询所有员工</a>
 </body>
 </html>
--- mybatis05_ssm/src/main/java/com/stanlong/mybatis/controller/EmployeeController.java
+++ mybatis05_ssm/src/main/java/com/stanlong/mybatis/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.stanlong.mybatis.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +13,11 @@ import com.stanlong.mybatis.service.EmployeeService;
 @Controller
 public class EmployeeController {
 	
+	@Autowired
 	EmployeeService employeeService;
 
 	@RequestMapping("/getemps")
-	public String emps(Map<String, Object> map){
+	public String getEmps(Map<String, Object> map){
 		List<Employee> emps = employeeService.getEmps();
 		map.put("allEmps", emps);
 		return "list";
