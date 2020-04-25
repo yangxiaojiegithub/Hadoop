992b5f1355708b01b79788044d9f67bdaeb60fca
StanLong <youlinbhz@sina.cn>
2020-04-25 21:59:45 +0800

    springboot04-web-crud

---
+++ SpringBoot/springboot04-web-crud/src/main/java/com/example/demo/com/stanlong/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.demo.com.stanlong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Map;
+
+/**
+ * @author 矢量
+ * @date 2020/4/25-20:17
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map){
+        // classpath:/templates/success.html
+        map.put("hello", "你好"); //查出一些数据在页面中展示
+        return "success";
+    }
+}
