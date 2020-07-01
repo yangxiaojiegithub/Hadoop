bea47b5de63431cbcd20f06e8b29084f40d9437d
StanLong <youlinbhz@sina.cn>
2020-07-02 00:05:10 +0800

    scala 入门程序

---
+++ Scala/HelloScala/src/main/scala/com/stanlong/TestScala.scala
@@ -0,0 +1,11 @@
+package com.stanlong
+
+/**
+  * @author 矢量
+  * @date 2020/7/1-23:28
+  */
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("HelloScala, idea...")
+  }
+}
